fd73197ff7fee826b41aa2648384e15fd247be59
Quentin Danneville <quentindanneville@MacBook-Pro-de-Quentin.local>
2021-10-04 11:58:15 +0200

    [Documentation][POST /users/] Added body properties to swagger to create a new user (#5)

--- src/modules/user/useCases/createUser/createUser.controller.ts
+++ src/modules/user/useCases/createUser/createUser.controller.ts
@@ -2,10 +2,27 @@ import { Body, Controller, Delete, Get, HttpCode, Param, Post } from '@nestjs/co
 import { CreateUser } from './createUser.service';
 import { CreateUserDto } from '../../dto/createUser.dto';
 
+import {
+    ApiBody,
+} from '@nestjs/swagger';
+
 @Controller('users')
 export class CreateUserController {
     constructor(private readonly createUser: CreateUser) { }
 
+    @ApiBody({
+        description: 'Create a new user | Password -> MinLength : 6 / MaxLength : 15',
+        schema: {
+            type: 'object',
+            properties: {
+                firstname: { type: 'string' },
+                lastname: { type: 'string' },
+                password: { type: 'string' },
+                username: { type: 'string' },
+                email: { type: 'string' },
+            },
+        },
+    })
     @Post()
     @HttpCode(201)
     async createPost(@Body() user: CreateUserDto) {
