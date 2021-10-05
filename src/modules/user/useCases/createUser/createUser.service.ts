f988369d8ff8afac63ad4f2c3604fece737e3b55
Quentin Danneville <quentindanneville@MacBook-Pro-de-Quentin.local>
2021-10-05 17:28:16 +0200

    [Mail] Fixed wrong email format with nodemailer error (#3)

--- src/modules/user/useCases/createUser/createUser.service.ts
+++ src/modules/user/useCases/createUser/createUser.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { UserRepository } from '../../user.repository';
 import { MailerService } from '@nestjs-modules/mailer';
 import * as bcrypt from 'bcrypt';
@@ -17,14 +17,19 @@ export class CreateUser {
         const user = await this.usersRepository.createUser(body);
 
         if (user) {
-            await this.mailerService
-                .sendMail({
-                    to: user.email,
-                    from: 'ton@pote.dev', // sender address
-                    subject: 'Inscription sur pote.dev ✔', // Subject line
-                    text: `Bienvenue ${user.firstname} sur pote.dev`, // plaintext body
-                    html: `<b>Bienvenue ${user.firstname} sur pote.dev</b>`, // HTML body content
-                })
+            try {
+                await this.mailerService
+                    .sendMail({
+                        to: user.email,
+                        from: 'ton@pote.dev', // sender address
+                        subject: 'Inscription sur pote.dev ✔', // Subject line
+                        text: `Bienvenue ${user.firstname} sur pote.dev`, // plaintext body
+                        html: `<b>Bienvenue ${user.firstname} sur pote.dev</b>`, // HTML body content
+                    })
+            }
+            catch (err) {
+                throw new BadRequestException("Wrong email format");
+            }
         }
 
         return user
