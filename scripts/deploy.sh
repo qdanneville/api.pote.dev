2231c8f2a5a863655453166e86397cefbdc4ac8d
quentindanneville <quentin.danneville@gmail.com>
2022-02-10 17:59:15 +0100

    [Devops] CI/CD with docker-compose (#17)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 ssh $SSH_USER@$SSH_SERVER << 'ENDSSH'
-    cd ~/app
+    cd ~/app/api
     docker login registry.gitlab.com -u $USERNAME -p $TOKEN
-    docker pull $IMAGE:postgres
-    docker pull $IMAGE:redis
     docker pull $IMAGE:nestjs
     docker pull $IMAGE:nginx
     docker-compose -f docker-compose.yml up -d --build
