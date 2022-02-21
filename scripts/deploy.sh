dc2e28968e896e61374d8feb3adf99a994656ef1
quentindanneville <quentin.danneville@gmail.com>
2022-02-21 19:05:43 +0100

    [Devops] Added prisma migrate & prisma studio along with correct env variable (#16)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,8 @@ ssh $SSH_USER@$SSH_SERVER << EOF
     docker pull $IMAGE:studio
     docker-compose -f docker-compose.yml down --remove-orphans
     docker pull $IMAGE:nestjs
-    docker-compose -f docker-compose.nginx up -d
+    docker-compose -f docker-compose.nginx.yml pull
+    docker-compose -f docker-compose.nginx.yml up -d
     docker-compose -f docker-compose.migrate.yml up -d
     docker-compose -f docker-compose.studio.yml up -d
     docker-compose -f docker-compose.yml up -d
